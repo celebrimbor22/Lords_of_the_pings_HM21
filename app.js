c8c1e4f540927cbf9a09655a65540549d5c0a566
francisco-cancino <A01730698@itesm.mx>
2021-08-29 11:08:03 -0500

    detalles 5 con app.js

--- app.js
+++ app.js
@@ -15,28 +15,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
       switch(emocion){
 
         case 'neutral': 
-        $("playlist").replaceWith('<iframe src="https://open.spotify.com/embed/playlist/0Irlk35VY2udpt1kBk0xhS?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+        $("playlist").append('<iframe src="https://open.spotify.com/embed/playlist/0Irlk35VY2udpt1kBk0xhS?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
             break;
         case 'happy':
-        $("playlist").replaceWith('<iframe src="https://open.spotify.com/embed/playlist/0Irlk35VY2udpt1kBk0xhS?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+        $("playlist").append('<iframe src="https://open.spotify.com/embed/playlist/0Irlk35VY2udpt1kBk0xhS?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
             break;
         case 'sad':
-        $("playlist").replaceWith('<iframe src="https://open.spotify.com/embed/playlist/0auIqTvjrk95HPE6mVGji3?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+        $("playlist").append('<iframe src="https://open.spotify.com/embed/playlist/0auIqTvjrk95HPE6mVGji3?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
             break;
         case 'angry':
-            $("playlist").replaceWith('<iframe src="https://open.spotify.com/embed/episode/0c6NqF0IEoX1fBypjdUQgK?theme=0" width="90%" height="152" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+            $("playlist").append('<iframe src="https://open.spotify.com/embed/episode/0c6NqF0IEoX1fBypjdUQgK?theme=0" width="90%" height="152" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
             break;
         case 'fearful':
-            $("playlist").replacWwith('<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWVtFO27AQ6dF?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+            $("playlist").append('<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWVtFO27AQ6dF?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
             break;
         case 'disgusted':
-            $("playlist").replaceWith('<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4gOolUyeDaQ?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>'); 
+            $("playlist").append('<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4gOolUyeDaQ?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>'); 
             break;
         case 'surprised':
-            $("playlist").replaceWith('<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DXd9zR7tdziuQ?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');    
+            $("playlist").append('<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DXd9zR7tdziuQ?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');    
             break;
         default: 
-        $("playlist").replaceWith('<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DXd9zR7tdziuQ?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+        $("playlist").append('<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DXd9zR7tdziuQ?theme=0" width="90%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
                 
         }
 
